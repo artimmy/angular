2bdc56073f75ffd189f1d7072aab758dc370aa73
artimmy <arthurtimmy@hotmail.com>
2019-09-17 11:37:54 -0300

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './principal/principal.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, PrincipalComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
